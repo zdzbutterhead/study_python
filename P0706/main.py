d75f61b201a170fb0617e7826821fb2d784e23d9
zdzbutterhead <zdz1411061965@163.com>
2025-07-15 22:08:54 +0800

    250715 22:08

--- P0706/main.py
+++ P0706/main.py
@@ -301,4 +301,37 @@ except UnicodeDecodeError:
 '''
 
 
+'''
+import json
 
+my_dict = {
+    'name': '骆昊',
+    'age': 40,
+    'friends': ['王大锤', '白元芳'],
+    'cars': [
+        {'brand': 'BMW', 'max_speed': 240},
+        {'brand': 'Audi', 'max_speed': 280},
+        {'brand': 'Benz', 'max_speed': 280}
+    ]
+}
+with open('data.json', 'w') as file:
+    json.dump(my_dict, file)
+
+with open('data.json', 'r') as file:
+    my_dict = json.load(file)
+    print(type(my_dict))
+    print(my_dict)
+'''
+
+
+'''
+import requests
+
+response = requests.get('https://apis.tianapi.com/guonei/index?key=99a9159476b792afa46184935a66047e&num=10')
+if response.status_code == 200:
+    data_model = response.json()
+    for news in data_model['result']['newslist']:
+        print(news['title'])
+        print(news['url'])
+        print('-' * 60)
+'''
